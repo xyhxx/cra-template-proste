960e445c747a0fdc2532beac488d49bbcf9534e4
肖 <603331487@qq.com>
2022-02-05 13:21:12 +0800

    format routes

--- template/src/routes/index.tsx
+++ template/src/routes/index.tsx
@@ -1,13 +1,2 @@
-import { FC } from 'react';
-import { App } from '@pages';
-import { useRoutes } from 'react-router';
-import { APP_PAGE } from './name';
-
-const PrivateRoutes: FC = () => {
-  const Routes = useRoutes([{ path: APP_PAGE, element: <App /> }]);
-
-  return Routes;
-};
-
-export default PrivateRoutes;
+export { default } from './routes';
 export * from './name';
---
+++ template/src/routes/routes.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { App } from '@pages';
+import { useRoutes } from 'react-router';
+import { APP_PAGE } from './name';
+
+const PrivateRoutes: FC = () => {
+  const Routes = useRoutes([{ path: APP_PAGE, element: <App /> }]);
+
+  return Routes;
+};
+
+export default PrivateRoutes;
